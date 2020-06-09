f4fdfa5e3844969a3c10b234b6161598ae830231
Utkarsh Singh <us9044@gmail.com>
2020-06-09 13:39:13 +0530

    week 4 assignment

---
+++ Coursera/week4-assignment/JS/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    $('#mycarousel').carousel({interval: 2000});
+    $('#carouselButton').click(function() {
+        if($('#carouselButton').children('span').hasClass('fa-pause'))
+        {
+            $('#mycarousel').carousel('pause');
+            $('#carouselButton').children('span').removeClass('fa-pause');
+            $('#carouselButton').children('span').addClass('fa-play');
+        } else 
+        {
+            $('#mycarousel').carousel('cycle');
+            $('#carouselButton').children('span').removeClass('fa-play');
+            $('#carouselButton').children('span').addClass('fa-pause');
+        };
+    });
+});
+
+$('#reserveButton').on('click',function() {
+    $('#reserveTableModal').modal('show');
+})
+
+$('#login').on('click',function() {
+    $('#loginModal').modal('show');
+})
